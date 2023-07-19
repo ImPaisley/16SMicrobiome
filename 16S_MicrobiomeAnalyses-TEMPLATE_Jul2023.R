e609a3de728fdbd0620e5989ac2571e499623ac1
Paise <paisley.samuel.98@gmail.com>
2023-07-18 21:57:02 -0400

    updated small things like removing spaces

--- 16S_MicrobiomeAnalyses-TEMPLATE_Jul2023.R
+++ 16S_MicrobiomeAnalyses-TEMPLATE_Jul2023.R
@@ -743,7 +743,7 @@ ef.cca<- envfit(cca.p,metadata[,c(7:10,12:19,21,23,24,31,33)]) # use the metadat
 # function (select.envfit) filters the resulting list of function (envfit) based on their p values. This allows to display only significant values in the final plot.
 # RUN FUNCTION FIRST!!
 select.envfit<-function(fit, r.select){ #needs two sorts of input: fit= result of envfit, r.select= numeric, correlation minimum threshold
-  for (i in 1:length(fit$vectors$r)) { #run for-loop through the entire length of the column r in object fit$vectors$r starting at i=1
+  for (i in 1:length(fit$vectors$r)) { #run for-loop through the entire length of the column r in object fit$vectors$r starting at i=1 # nolint # nolint # nolint
     if (fit$vectors$r[i]<r.select) { #Check whether r<r.select, i.e. if the correlation is weaker than the threshold value. Change this Parameter for r-based selection
       fit$vectors$arrows[i,]=NA #If the above statement is TRUE, i.e. r is smaller than r.select, then the coordinates of the vectors are set to NA, so they cannot be displayed
       i=i+1 #increase the running parameter i from 1 to 2, i.e. check the next value in the column until every value has been checked
